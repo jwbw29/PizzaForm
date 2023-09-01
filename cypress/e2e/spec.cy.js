a2e7a41d0e0d6164020ad7373a6891adec8225b3
Justin Byrd <justinbyrd7@gmail.com>
2023-09-01 18:12:15 -0500

    wip

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,23 +1,26 @@
 describe("Pizza Form", () => {
   beforeEach(() => {
+    // eslint-disable-next-line no-undef
     cy.visit("http://localhost:3000");
   });
+  // [x]  SANITY CHECK //////
+  it("sanity check", () => {
+    expect(1 + 2).to.equal(3);
+  });
+
+  // [ ] HELPERS ///////
+
+  // [ ] PROPER ELEMENTS ARE SHOWING ///
+
+  // [ ] MVP - FILLING OUT INPUTS /////
+
+  // [ ] MVP - SELECT MULTIPLE TOPPINGS ////
+
+  // [ ] MVP - SUBMITTING FORM DATA //////
+
+  // [ ] CAN ONLY SELECT ONE SAUCE
+
+  // [ ] ERROR HANDLING /////
+
+  // [ ] SUBMITTING FORM DATA //////
 });
-
-// [ ] HELPERS ///////
-
-// [ ]  SANITY CHECK //////
-
-// [ ] PROPER ELEMENTS ARE SHOWING ///
-
-// [ ] MVP - FILLING OUT INPUTS /////
-
-// [ ] MVP - SELECT MULTIPLE TOPPINGS ////
-
-// [ ] MVP - SUBMITTING FORM DATA //////
-
-// [ ] CAN ONLY SELECT ONE SAUCE
-
-// [ ] ERROR HANDLING /////
-
-// [ ] SUBMITTING FORM DATA //////
